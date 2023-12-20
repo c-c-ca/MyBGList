f7329dbd07410bb58b1f388127fc8f141a841826
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-20 15:05:59 -0500

    Add CORS default policy and any-origin policy

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(cfg =>
+        {
+            cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+        options.AddPolicy(name: "AnyOrigin", cfg =>
+        {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+    }
+);
 
 var app = builder.Build();
 
@@ -27,10 +46,16 @@ else
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
-app.MapGet("/error", () => Results.Problem());
-app.MapGet("/error/test", () => { throw new Exception("test"); });
+// Minimal API
+app.MapGet("/error", [EnableCors("AnyOrigin")] () => 
+    Results.Problem());
+
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => 
+    { throw new Exception("test"); });
 
 app.MapControllers();
 
