4ee38ffe769ab8b01cb951dba0e04b98262a61c9
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-25 11:55:58 -0500

    Implement the IValidatableObject to the DomainDTO object so that the model is only valid if the Id is 3 or the Name is "Wargames"

--- MyBGList/DTO/DomainDTO.cs
+++ MyBGList/DTO/DomainDTO.cs
@@ -4,12 +4,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyBGList.DTO
 {
-    public class DomainDTO
+    public class DomainDTO : IValidatableObject
     {
         [Required]
         public int Id { get; set; }
 
         [LettersOnlyValidator(UseRegex = true)]
         public string? Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(
+            ValidationContext validationContext)
+        {
+            return Id == 3 || Name == "Wargames" ?
+                new ValidationResult[0] :
+                new [] { 
+                    new ValidationResult("Id and/or Name values must match an allowed Domain.") 
+                };
+        }
     }
 }
