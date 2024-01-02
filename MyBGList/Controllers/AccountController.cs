8a10fc40ec513cee63cca7083b91a867b4a7faf1
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-02 17:39:43 -0500

    Add AccountController

---
+++ MyBGList/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBGList.DTO;
+using MyBGList.Models;
+using System.Linq.Expressions;
+using System.Linq.Dynamic.Core;
+using System.ComponentModel.DataAnnotations;
+using MyBGList.Attributes;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        private readonly ILogger<DomainsController> _logger;
+
+        private readonly IConfiguration _configuration;
+
+        private readonly UserManager<ApiUser> _userManager;
+
+        private readonly SignInManager<ApiUser> _signInManager;
+
+        public AccountController(
+            ApplicationDBContext context,
+            ILogger<DomainsController> logger,
+            IConfiguration configuration,
+            UserManager<ApiUser> userManager,
+            SignInManager<ApiUser> signInManager)
+        {
+            _context = context;
+            _logger = logger;
+            _configuration = configuration;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ResponseCache(CacheProfileName = "NoCache")]
+        public async Task<ActionResult> Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ResponseCache(CacheProfileName = "NoCache")]
+        public async Task<ActionResult> Login()
+        {
+            throw new NotSupportedException();
+        }
+
+    }
+}
