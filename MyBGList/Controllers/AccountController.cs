bdf1916538db69eb5a8f9875d8f7f6d85c1477c4
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-03 10:43:47 -0500

    Add AccountController.Login action method

--- MyBGList/Controllers/AccountController.cs
+++ MyBGList/Controllers/AccountController.cs
@@ -95,9 +95,60 @@ namespace MyBGList.Controllers
 
         [HttpPost]
         [ResponseCache(CacheProfileName = "NoCache")]
-        public async Task<ActionResult> Login()
+        public async Task<ActionResult> Login(LoginDTO input)
         {
-            throw new NotSupportedException();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = await _userManager.FindByNameAsync(input.UserName);
+                    if (user == null || 
+                        !await _userManager.CheckPasswordAsync(user, input.Password))
+                    {
+                        throw new Exception("Invalid login attempt.");
+                    }
+                    else
+                    {
+                        var signingCredentials = new SigningCredentials(
+                            new SymmetricSecurityKey(
+                                System.Text.Encoding.UTF8.GetBytes(
+                                    _configuration["JWT:SigningKey"])),
+                            SecurityAlgorithms.HmacSha256);
+
+                        var claims = new List<Claim>();
+                        claims.Add(new Claim(ClaimTypes.Name, input.UserName));
+
+                        var jwtObject = new JwtSecurityToken(
+                            issuer: _configuration["JWT:Issuer"],
+                            audience: _configuration["JWT:Audience"],
+                            claims: claims,
+                            expires: DateTime.Now.AddSeconds(300),
+                            signingCredentials: signingCredentials);
+
+                        var jwtString = new JwtSecurityTokenHandler()
+                            .WriteToken(jwtObject);
+
+                        return StatusCode(StatusCodes.Status200OK, jwtString);
+                    }
+                }
+                else
+                {
+                    var details = new ValidationProblemDetails(ModelState);
+                    details.Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+                    details.Status = StatusCodes.Status400BadRequest;
+                    return new BadRequestObjectResult(details);
+                }
+            }
+            catch (Exception e)
+            {
+                var exceptionDetails = new ProblemDetails();
+                exceptionDetails.Detail = e.Message;
+                exceptionDetails.Status = StatusCodes.Status401Unauthorized;
+                exceptionDetails.Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+                return StatusCode(
+                    StatusCodes.Status401Unauthorized,
+                    exceptionDetails);
+            }
         }
 
     }
