8733a0f5ee5e33cf276c111d551006b0bfcac194
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-09 10:31:20 -0500

    Add a gRPC client to test the gRPC service

---
+++ MyBGList/Controllers/GrpcController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Grpc.Net.Client;
+using MyBGList.gRPC;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class GrpcController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<BoardGameResponse> GetBoardGame(int id)
+        {
+            using var channel = GrpcChannel
+                .ForAddress("https://localhost:40443");
+            var client = new gRPC.Grpc.GrpcClient(channel);
+            var response = await client.GetBoardGameAsync(
+                                new BoardGameRequest { Id = id });
+            return response;
+        }
+    }
+}
