fb69c22b5e39269aaa5368f46e134d7f69c13dad
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-09 11:03:02 -0500

    Add REST endpoint to test gRPC UpdateBoardGame endpoint

--- MyBGList/Controllers/GrpcController.cs
+++ MyBGList/Controllers/GrpcController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Grpc.Core;
 using Grpc.Net.Client;
 using MyBGList.gRPC;
 
@@ -16,6 +17,28 @@ namespace MyBGList.Controllers
             var client = new gRPC.Grpc.GrpcClient(channel);
             var response = await client.GetBoardGameAsync(
                                 new BoardGameRequest { Id = id });
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<BoardGameResponse> UpdateBoardGame(
+            string token,
+            int id,
+            string name)
+        {
+            var headers = new Metadata();
+            headers.Add("Authorization", $"Bearer {token}");
+
+            using var channel = GrpcChannel
+                .ForAddress("https://localhost:40443");
+            var client = new gRPC.Grpc.GrpcClient(channel);
+            var response = await client.UpdateBoardGameAsync(
+                new UpdateBoardGameRequest
+                {
+                    Id = id,
+                    Name = name
+                },
+                headers);
             return response;
         }
     }
